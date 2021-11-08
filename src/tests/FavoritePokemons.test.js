d7e331ab034870e14520ca10e87d2f31658a4202
Miguel_Reis <guelsouzar@gmail.com>
2021-11-08 16:56:10 -0300

    Requisto 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,36 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router'; //! Router
+/* import { createMemoryHistory } from 'history';
+import userEvent from '@testing-library/user-event'; */
+import userEvent from '@testing-library/user-event';
+import { FavoritePokemons } from '../components';
+import App from '../App';
+
+const { render, screen } = require('@testing-library/react');
+
+describe('Requisto 3', () => {
+  it(' No favorite pokemon found caso não tiver pokémons favoritos.', () => {
+    render(
+      <MemoryRouter>
+        <FavoritePokemons />
+      </MemoryRouter>,
+    );
+    const notFound = screen.getByText(/No favorite pokemon found/i);
+    expect(notFound).toBeInTheDocument();
+  });
+  it('Teste se é exibido todos os cards de pokémons favoritados.', () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const linkCard = screen.getByRole('link', { name: /More details/i });
+    userEvent.click(linkCard);
+    const labelCard = screen.getByLabelText(/Pokémon favoritado?/i);
+    userEvent.click(labelCard);
+    const linkFavorite = screen.getByRole('link', { name: /Favorite Pokémons/i });
+    userEvent.click(linkFavorite);
+    const notFound = screen.queryByText(/No favorite pokemon found/i);
+    expect(notFound).not.toBeInTheDocument();
+  });
+});
